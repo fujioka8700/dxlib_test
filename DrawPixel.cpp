d2fe3d79adbbbdb4d5a9e2823bff30aa72e271bf
Masahiro Fujioka <fujioka8700@gmail.com>
2024-03-16 11:59:48 +0900

    プロジェクト ファイルを追加します。

---
+++ DrawPixel.cpp
@@ -0,0 +1,22 @@
+#include "DxLib.h"
+
+// プログラムは WinMain から始まります
+int WINAPI WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_  HINSTANCE hPrevInstance,
+	_In_ LPSTR lpCmdLine,
+	_In_ int nShowCmd)
+{
+	if (DxLib_Init() == -1) // ＤＸライブラリ初期化処理
+	{
+		return -1;			// エラーが起きたら直ちに終了
+	}
+
+	DrawPixel(320, 240, GetColor(255, 255, 255));	// 点を打つ
+
+	WaitKey();				// キー入力待ち
+
+	DxLib_End();			// ＤＸライブラリ使用の終了処理
+
+	return 0;				// ソフトの終了 
+}

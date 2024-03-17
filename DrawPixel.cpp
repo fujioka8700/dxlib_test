72e78cf91de6f51329bbaa0384521de859f0e6ec
Masahiro Fujioka <fujioka8700@gmail.com>
2024-03-17 20:38:08 +0900

    [update] ディスクアクセスする。

--- DrawPixel.cpp
+++ DrawPixel.cpp
@@ -11,18 +11,13 @@ int WINAPI WinMain(
 	SetGraphMode(640, 480, 32);           //画面サイズ指定
 	SetOutApplicationLogValidFlag(FALSE); //Log.txtを生成しないように設定
 
-	int GraphHandle;
-
 	if (DxLib_Init() == -1) // ＤＸライブラリ初期化処理
 	{
 		return -1;			// エラーが起きたら直ちに終了
 	}
 
-	// グラフィック『test1.bmp』をメモリにロード
-	GraphHandle = LoadGraph("test1.bmp");
-
-	// 0,0 座標にメモリに読みこんだグラフィックを描画
-	DrawGraph(0, 0, GraphHandle, FALSE);
+	// 0,0 座標にグラフィック『test1.bmp』を描画
+	LoadGraphScreen(0, 0, "test1.bmp", FALSE);
 
 	// キー入力を待つ
 	WaitKey();

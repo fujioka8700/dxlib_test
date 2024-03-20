d9f052dacd167f43f999bfb826a40f17ec8c9a47
Masahiro Fujioka <fujioka8700@gmail.com>
2024-03-20 19:12:51 +0900

    [update] 自機を操作できるようにしよう

--- DrawPixel.cpp
+++ DrawPixel.cpp
@@ -8,8 +8,7 @@ int WINAPI WinMain(
 	_In_ int nShowCmd)
 {
 	int BallX, BallY, BallGraph;
-	int SikakuX, SikakuY, SikakuGraph;
-	int i;
+	int SikakuX, SikakuY, SikakuMuki, SikakuGraph;
 
 	ChangeWindowMode(TRUE);               //非全画面にセット
 	SetGraphMode(640, 480, 16);           //画面サイズ指定
@@ -20,48 +19,94 @@ int WINAPI WinMain(
 		return -1;			// エラーが起きたら直ちに終了
 	}
 
-	// 描画先を裏画面にする
+	// グラフィックの描画先を裏画面にセット
 	SetDrawScreen(DX_SCREEN_BACK);
 
 	// ボール君のグラフィックをメモリにロード＆表示座標をセット
 	BallGraph = LoadGraph("Ball.png");
-	BallX = -64; BallY = 0;
+	BallX = 288; BallY = 400;
 
 	// 四角君のグラフィックをメモリにロード＆表示座標をセット
 	SikakuGraph = LoadGraph("Sikaku.png");
-	SikakuX = 640; SikakuY = 300;
+	SikakuX = 0; SikakuY = 50;
+
+	// 四角君の移動方向をセット
+	SikakuMuki = 1;
 
 	// 移動ルーチン
-	for (i = 0; i < 400; i++)
+	while (1)
 	{
 		// 画面を初期化(真っ黒にする)
 		ClearDrawScreen();
 
-		// ボール君の座標を少しずらす
-		BallX += 3;
+		// ボール君の操作ルーチン
+		{
+			// ↑キーを押していたらボール君を上に移動させる
+			if (CheckHitKey(KEY_INPUT_UP) == 1) BallY -= 3;
 
-		// ボール君を描画
-		DrawGraph(BallX, BallY, BallGraph, FALSE);
+			// ↓キーを押していたらボール君を下に移動させる
+			if (CheckHitKey(KEY_INPUT_DOWN) == 1) BallY += 3;
 
-		// 四角君の座標を少しずらす
-		SikakuX -= 3;
-		SikakuY -= 2;
+			// ←キーを押していたらボール君を左に移動させる
+			if (CheckHitKey(KEY_INPUT_LEFT) == 1) BallX -= 3;
 
-		// 四角君を描画
-		DrawGraph(SikakuX, SikakuY, SikakuGraph, FALSE);
+			// →キーを押していたらボール君を右に移動させる
+			if (CheckHitKey(KEY_INPUT_RIGHT) == 1) BallX += 3;
+
+			// ボール君が画面左端からはみ出そうになっていたら画面内の座標に戻してあげる
+			if (BallX < 0) BallX = 0;
+
+			// ボール君が画面右端からはみ出そうになっていたら画面内の座標に戻してあげる
+			if (BallX > 640 - 64) BallX = 640 - 64;
+
+			// ボール君が画面上端からはみ出そうになっていたら画面内の座標に戻してあげる
+			if (BallY < 0) BallY = 0;
+
+			// ボール君が画面下端からはみ出そうになっていたら画面内の座標に戻してあげる
+			if (BallY > 480 - 64) BallY = 480 - 64;
+
+			// ボール君を描画
+			DrawGraph(BallX, BallY, BallGraph, FALSE);
+		}
+
+
+		// 四角君の移動ルーチン
+		{
+			// 四角君の座標を移動している方向に移動する
+			if (SikakuMuki == 1) SikakuX += 3;
+			if (SikakuMuki == 0) SikakuX -= 3;
+
+			// 四角君が画面右端からでそうになっていたら画面内の座標に戻してあげ、移動する方向も反転する
+			if (SikakuX > 576)
+			{
+				SikakuX = 576;
+				SikakuMuki = 0;
+			}
+
+			// 四角君が画面左端からでそうになっていたら画面内の座標に戻してあげ、移動する方向も反転する
+			if (SikakuX < 0)
+			{
+				SikakuX = 0;
+				SikakuMuki = 1;
+			}
+
+			// 四角君を描画
+			DrawGraph(SikakuX, SikakuY, SikakuGraph, FALSE);
+		}
 
 		// 裏画面の内容を表画面にコピーする
 		ScreenFlip();
+
 
 		// Windows 特有の面倒な処理をＤＸライブラリにやらせる
 		// -1 が返ってきたらループを抜ける
 		if (ProcessMessage() < 0) break;
 
-		// ＥＳＣキーが押されたらループから抜ける
-		if (CheckHitKey(KEY_INPUT_ESCAPE) == 1) break;
+		// もしＥＳＣキーが押されていたらループから抜ける
+		if (CheckHitKey(KEY_INPUT_ESCAPE)) break;
 	}
 
-	DxLib_End();			// ＤＸライブラリ使用の終了処理
+	DxLib_End();				// ＤＸライブラリ使用の終了処理
 
-	return 0;				// ソフトの終了 
+	return 0;					// ソフトの終了
 }
